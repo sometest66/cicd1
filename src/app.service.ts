b92287166a03e63eabafe4ea61d98c3d150e0f5a
sometest66 <sometest66@gmail.com>
2025-03-12 01:28:06 +0545

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! For git webhook trigger to coolify';
+    return 'Hello World! For git webhook trigger to coolify. added secret';
   }
 }
